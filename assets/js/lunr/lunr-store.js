3af93ba838cec42ea24b57738522d74f9e0f95fb
tim-herpich <tim-herpich@users.noreply.github.com>
2025-08-22 20:37:41 +0000

    deploy: 2494665e909efc568bd90b39164000f7c5efea09

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,17 +1,17 @@
 var store = [{
-        "title": "Beyond Beta: Hunting for the Perfect Portfolio",
+        "title": "1. Beyond Beta: Hunting for the Perfect Portfolio",
         "excerpt":"The History of Capital Market Research The question of what constitutes an optimal investment portfolio has been at the heart of capital market research for decades. Traditionally, the Capital Asset Pricing Model (CAPM), developed by William Sharpe and based on Harry Markowitz’s Modern Portfolio Theory, posits that the market portfolio—a...","categories": ["Finance","Risk Management","Capital Market Research"],
         "tags": ["Solvency II","risk"],
         "url": "/finance/risk%20management/capital%20market%20research/2024/01/01/FactorInvesting.html",
         "teaser": null
       },{
-        "title": "MBA: Lessons Beyond the Books",
+        "title": "2. MBA: Lessons Beyond the Books",
         "excerpt":"The MBA Journey Embarking on the two-year Mannheim Business School MBA journey has been a truly transformative experience — both personally and professionally. What stood out most was the uniquely diverse and international environment, where classmates from varied professional, cultural, and personal backgrounds came together. This diversity enriched every discussion...","categories": ["Finance","Risk Management","Marketing"],
         "tags": ["risk","marketing","emobility","MBA"],
         "url": "/finance/risk%20management/marketing/2024/08/20/MbaDefense.html",
         "teaser": null
       },{
-        "title": "Dealing with the Non-Marketable: Export Credit Insurances",
+        "title": "3. Dealing with the Non-Marketable: Export Credit Insurances",
         "excerpt":"The Challenge Export Credit Agencies (ECAs) insure risks that commercial insurers avoid: long-term, high-risk, and often politically uncertain transactions. While pricing export credit insurance policies is already complex, pricing modifications to these policies is an even greater challenge. There are no regulatory guidelines, no established market benchmarks, and each export...","categories": ["Finance","Risk Management"],
         "tags": ["export credit insurance","pricing","risk","ECAs"],
         "url": "/finance/risk%20management/2024/10/03/ServWhitepaper.html",
