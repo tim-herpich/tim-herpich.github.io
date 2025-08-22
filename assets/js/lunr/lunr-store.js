835ef175be2b4d9e1c7f228c9009bea948c1c83d
tim-herpich <tim-herpich@users.noreply.github.com>
2025-08-22 12:07:25 +0000

    deploy: db69ebb6a81c0ece00651968e96ab7196dab3d7d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,157 @@
+var store = [{
+        "title": "Beyond Beta: Hunting for the Perfect Portfolio",
+        "excerpt":"The History of Capital Market Research The question of what constitutes an optimal investment portfolio has been at the heart of capital market research for decades. Traditionally, the Capital Asset Pricing Model (CAPM), developed by William Sharpe and based on Harry Markowitz’s Modern Portfolio Theory, posits that the market portfolio—a...","categories": ["Finance","Risk Management","Capital Market Research"],
+        "tags": ["Solvency II","risk"],
+        "url": "/finance/risk%20management/capital%20market%20research/2024/01/01/FactorInvesting.html",
+        "teaser": null
+      },{
+        "title": "MBA: Lessons Beyond the Books",
+        "excerpt":"The MBA Journey Embarking on the two-year Mannheim Business School MBA journey has been a truly transformative experience — both personally and professionally. What stood out most was the uniquely diverse and international environment, where classmates from varied professional, cultural, and personal backgrounds came together. This diversity enriched every discussion...","categories": ["Finance","Risk Management","Marketing"],
+        "tags": ["risk","marketing","emobility","MBA"],
+        "url": "/finance/risk%20management/marketing/2024/08/20/MbaDefense.html",
+        "teaser": null
+      },{
+        "title": "Dealing with the Non-Marketable: Export Credit Insurances",
+        "excerpt":"The Challenge Export Credit Agencies (ECAs) insure risks that commercial insurers avoid: long-term, high-risk, and often politically uncertain transactions. While pricing export credit insurance policies is already complex, pricing modifications to these policies is an even greater challenge. There are no regulatory guidelines, no established market benchmarks, and each export...","categories": ["Finance","Risk Management"],
+        "tags": ["export credit insurance","pricing","risk","ECAs"],
+        "url": "/finance/risk%20management/2024/10/03/ServWhitepaper.html",
+        "teaser": null
+      },{
+        "title": "Collective Power: Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/18Prx/",
+        "teaser": null
+      },{
+        "title": "Thermodynamics of majority-logic decoding in information erasure",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/19Entropy/",
+        "teaser": null
+      },{
+        "title": "Universality in driven Potts models",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/19Pre/",
+        "teaser": null
+      },{
+        "title": "Effective thermodynamics of two interacting underdamped Brownian particles",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/19PreII/",
+        "teaser": null
+      },{
+        "title": "Has (multi-)factor investing become obsolete?",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/20FactorInvesting/",
+        "teaser": null
+      },{
+        "title": "Stochastic thermodynamics of all-to-all interacting many-body systems",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/20Njp/",
+        "teaser": null
+      },{
+        "title": "Dealing With The Non-Marketable: A Unified Pricing Framework for Modifications of Export Credit Insurances",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/24Serv/",
+        "teaser": null
+      },{
+        "title": "Effekte der Tachyonenphysik",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/Bsc/",
+        "teaser": null
+      },{
+        "title": "Development of an Innovative Strategy to Leverage Marketing Potentials on Ionity Charging Locations",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/Mba/",
+        "teaser": null
+      },{
+        "title": "Linear and nonlinear microrheology",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/Msc/",
+        "teaser": null
+      },{
+        "title": " Collective Effects in Stochastic Thermodynamics",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/items/PhD/",
+        "teaser": null
+      },{
+        "title": " Stochastic Thermodynamics of Coupled Phase Oscillators",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/17Palma/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18Woeltingerode/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18badhonnef/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18berlin/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18luxembourg/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18netherlands/",
+        "teaser": null
+      },{
+        "title": "Minimal Model for Thermodynamics of Nonequilibrium Phase Transitions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/18potsdam/",
+        "teaser": null
+      },{
+        "title": "Thermodynamics of Nonequilibrium Phase Transitions in Driven Potts Models",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/19Boston/",
+        "teaser": null
+      },{
+        "title": "Thermodynamics of Nonequilibrium Phase Transitions in Driven Potts Models",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/19SanDiego/",
+        "teaser": null
+      },{
+        "title": "Thermodynamics of Nonequilibrium Phase Transitions in Driven Potts Models",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/19bolivia/",
+        "teaser": null
+      },{
+        "title": "Stochastic Thermodynamics of the Potts Model",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/19thailand/",
+        "teaser": null
+      },{
+        "title": " An expert session on pricing issues",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/items/24serv/",
+        "teaser": null
+      }]
